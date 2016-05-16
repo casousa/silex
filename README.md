6d5139f04a7a995ee4769d3a48fb50c656a7c88e
Caio Andrade <caioandradedesousa@gmail.com>
2016-05-15 22:23:30 -0300

    Segundo commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Silex
 
 - Pequeno projeto para aprendizado do micro-framework Silex na versão 1.2.5
+
+
+####  Server
+
+- Para rodar utilizando o servidor embutido do PHP, disponível a partir da versão 5.4:
+ - php -S localhost:8080 -t web web/index.php
--- web/index.php
+++ web/index.php
@@ -1,17 +1,25 @@
 <?php
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;]
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
+$app->before(function(Request $request){
+	print("Antes das rotas");
+});
+
 $app->get('/hello/{name}', function ($name) use ($app) {
   return 'Hello '.$app->escape($name);
 });
 
+$app->after(function(Request $request, Response $response){
+	print("Middleware After");
+});
 
-//$app->get('/', function () {
-  //return 'Hello ';
-//});
 $app->run();
