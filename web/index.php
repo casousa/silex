906cc8e4f0187859df01fe3afcf37c2e7651d5d2
Caio Andrade <caioandradedesousa@gmail.com>
2016-05-16 22:24:43 -0300

    Setimo Commit

--- web/index.php
+++ web/index.php
@@ -23,28 +23,3 @@ $app->get('/', function () use ($app) {
 $app->mount('/users', require __DIR__.'/../src/Users/Controller/IndexController.php');
 
 $app->run();
-
-
-/*
- * COMANDOS COMENTADOS APENAS PARA POSTERIOR CONSULTA
- */
-
-//Utilizado para alterar valor de retorno da função da rota
-//->convert('name', function($name){ // Converte o tipo do valor retornado
-	//return 	(int) $name; 
-//});
-
-//Executado antes da rota
-//$app->before(function(Request $request){
-	//print("Antes das rotas.");
-//});
-
-//Executado depois da rota
-//$app->after(function(Request $request, Response $response){
-	//print(" <br> Middleware After. <br>");
-//});
-
-//Excutado após o response ser enviado para o browser
-//$app->finish(function(Request $request, Response $response){
-	//print(" <br>Vai ser executado depois que o response for enviado para o browser.");
-//});
